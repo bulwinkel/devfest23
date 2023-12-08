2f9187a0c2bcdd32a8dc28bbb0f9100a97c99277
Kelvin Bulwinkel <kelvin@bulwinkel.com>
2023-12-08 12:51:05 +1000

    4. Record audio

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,5 +1,15 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_sound_record/flutter_sound_record.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
+enum ChatStatus {
+  idle,
+  recording,
+  processing,
+}
 
 class ChatPage extends HookWidget {
   const ChatPage({super.key, required this.title});
@@ -8,9 +18,39 @@ class ChatPage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final counterState = useState(0);
-    final counter = counterState.value;
-    void incrementCounter() => counterState.value++;
+    final recorder = useMemoized(FlutterSoundRecord.new);
+    final player = useMemoized(AudioPlayer.new);
+
+    final status = useState(ChatStatus.idle);
+
+    Future<void> onFabPressed() async {
+      final dir = await getApplicationDocumentsDirectory();
+      final path = dir.path;
+      final recordingPath = join(path, 'recording.mp4');
+
+      switch (status.value) {
+        case ChatStatus.idle:
+          status.value = ChatStatus.recording;
+          await recorder.start(
+            path: recordingPath,
+          );
+          break;
+        case ChatStatus.recording:
+          status.value = ChatStatus.processing;
+          await recorder.stop();
+          status.value = ChatStatus.idle;
+          break;
+        case ChatStatus.processing:
+          break;
+      }
+    }
+
+    Future<void> play() async {
+      final dir = await getApplicationDocumentsDirectory();
+      final path = dir.path;
+      final recordingPath = join(path, 'recording.mp4');
+      await player.play(DeviceFileSource(recordingPath));
+    }
 
     return Scaffold(
       appBar: AppBar(
@@ -21,20 +61,23 @@ class ChatPage extends HookWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'Push me to increment:',
-            ),
-            Text(
-              '$counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            TextButton(
+              onPressed: play,
+              child: Text(
+                'Push to play',
+              ),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: incrementCounter,
+        onPressed: onFabPressed,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: switch (status.value) {
+          ChatStatus.idle => Icon(Icons.mic),
+          ChatStatus.recording => Icon(Icons.stop),
+          ChatStatus.processing => CircularProgressIndicator(),
+        },
       ),
     );
   }
